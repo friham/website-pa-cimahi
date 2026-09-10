b45d6311300ec69bc41223d52b79a20f44e0bed4
Belly Embun <bellyembun69@gmail.com>
2026-09-10 09:33:53 +0700

    Delete pacimahi directory

--- pacimahi/backend/routes/auditRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authMiddleware = require('../middleware/auth');
-const { checkRole } = require('../middleware/auth');
-const { getAuditLogs } = require('../controllers/auditLogController');
-
-router.get('/', authMiddleware, checkRole(['superadmin', 'admin']), getAuditLogs);
-
-module.exports = router;
